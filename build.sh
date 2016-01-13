a07587bb70039c35381cb28cd199b930c920a47d
Peter Fors <vtlmks@gmail.com>
2016-01-13 21:24:08 +0100

    Silence xorrisos output spewage..

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ PATH=~/x-tools/i386-kaos-elf/bin/:$PATH
 
 nasm -felf32 boot.s -o boot.o
 
-#i386-kaos-elf-gcc -c kernel.cpp -o kernel.o -ffreestanding -std=gnu99 -O3 -Wall -Wextra
-i386-kaos-elf-gcc -c kernel.cpp -o kernel.o -ffreestanding -O3 -Wall -Wextra
+i386-kaos-elf-gcc -c kernel.cpp -o kernel.o -ffreestanding -O3 -Wall -Wextra -Wunused-but-set-variable
 i386-kaos-elf-gcc -T linker.ld -o kaos.bin -ffreestanding -O3 -nostdlib boot.o kernel.o -lgcc
 
 echo "DONE."
