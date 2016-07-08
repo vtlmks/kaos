402cd2ee92b24b53fa9bccd5932512d9e9c26a24
Robert Falk <Robert Falk>
2016-07-08 13:53:11 +0200

    Postbuild fix

--- loader/kernel/scripts/postbuild.bat
+++ loader/kernel/scripts/postbuild.bat
@@ -1,2 +1,2 @@
-c:\cygwin64\bin\ld -T ..\linker.ld -e ?loader@@YAXXZ ..\x64\debug\loader.lib -o ..\x64\debug\loader.exe
-c:\cygwin64\bin\objcopy -S -O binary ..\x64\debug\loader.exe ..\x64\debug\kernel.bin
+c:\cygwin64\bin\ld -T ..\linker.ld -e ?loader@@YAXXZ ..\x64\debug\kernel.lib -o ..\x64\debug\kernel.exe
+c:\cygwin64\bin\objcopy -S -O binary ..\x64\debug\kernel.exe ..\x64\debug\kernel.bin
