2bca5136411e8ad985dddad489999cb4e01093cf
Robert Falk <Robert Falk>
2016-07-11 22:23:30 +0200

    Added copy of kernel bin to root directory.

--- loader/kernel/scripts/postbuild.bat
+++ loader/kernel/scripts/postbuild.bat
@@ -1,5 +1,4 @@
 rem c:\cygwin64\bin\ld -T ..\linker.ld x64\debug\kernel.obj -o ..\x64\debug\kernel.exe
-
 c:\cygwin64\bin\ld -Map ..\x64\debug\kernel.map -T ..\linker.ld x64\debug\load.obj x64\debug\kernel.obj -o ..\x64\debug\kernel.exe
-
 c:\cygwin64\bin\objcopy -S -O binary ..\x64\debug\kernel.exe ..\x64\debug\kernel.bin
+copy ..\x64\debug\kernel.bin ..\..\
