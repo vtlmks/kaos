ef8ed544729c9a23b5bdf5f0854bf86645948441
Peter Fors <vtlmks@gmail.com>
2016-07-13 13:41:28 +0200

    moved some headerfiles

--- build.bat
+++ build.bat
@@ -1,7 +1,13 @@
 @echo off
+
 nasm -fbin bootblock.asm -o bootblock.bin
+
 nasm -fbin loader.asm -o loader.bin
+
 cd kernel
+
 call build.bat
+
 cd ..
+
 bootimage bootblock.bin loader.bin kernel/kernel.bin
--- kernel/build.bat
+++ kernel/build.bat
@@ -1,4 +1,9 @@
+@echo off
 x86_64-elf-gcc -I./include -Xlinker -n -c kernel.cpp -o kernel.o -ffreestanding -O0 -std=c++11 -Wall -Wextra -fno-exceptions -fno-rtti
+
 x86_64-elf-gcc -I./include -Xlinker -n -c tty.cpp -o tty.o -ffreestanding -O0 -std=c++11 -Wall -Wextra -fno-exceptions -fno-rtti
+
 x86_64-elf-gcc -I./include -Xlinker -n -T linker.ld -o kernel.bin -ffreestanding -O0 -nostdlib kernel.o tty.o -lgcc -fno-exceptions -fno-rtti
+
 rem objcopy -S -O binary kaos.bin kernel.bin
+
